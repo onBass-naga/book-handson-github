1eb241ce92118d701390c5fa3a7e80c5805343d8
naga <naga@area-b.com>
2024-12-28 22:15:30 +0900

    fix

--- .github/workflows/scripts/log-level.js
+++ .github/workflows/scripts/log-level.js
@@ -3,7 +3,7 @@ module.exports = async (github) => {
   const labelName = github?.event?.label?.name
   console.log(labelName)
   const level =
-    labelName == null || !lastName.startsWith('log-level/')
+    !labelName || !lastName.startsWith('log-level/')
       ? 'info'
       : labelName.replace('log-level/', '')
 
